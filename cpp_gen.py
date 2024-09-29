8f7ee9943afc3f30cbf06dcf9906be42004d4ea2
Tom <tom@tschoenfelder.de>
2024-09-29 20:55:49 +0200

    WIP: Autogen msg_protocol.h

--- cpp_gen.py
+++ cpp_gen.py
@@ -7,28 +7,28 @@ class CppGen:
         with open(cfg_path, 'r') as f:
             self._cfg = json.load(f)
 
+        self._code = {}
+
+        # PROTOCOL_H_MSGIDS
+        self._code["PROTOCOL_H_MSGIDS"] = ""
+        last_fun = None
+        for f_name, f_data in self._cfg["functions"].items():
+            last_fun = f_name
+            self._code["PROTOCOL_H_MSGIDS"] += f"    MSGID_{f_name},\n"
+        self._code["PROTOCOL_H_MSGIDS"] += f"    MSGID_LAST = MSGID_{last_fun}\n"
+
+
+        # BRIDGE_INCLUDE
+        header_name = os.path.basename(self._cfg["header"])
+        self._code['BRIDGE_INCLUDE'] = f'#include "{header_name}"\n'
+
+        # BRIDGE_CALLBACK_DECL
+        bridge_cb_decl = ""
         self._callbacks = {}
         for cb in self._cfg['callbacks']:
-            self._callbacks[cb] = cb + "_inst"
-        
-    def code_gen(self, gen_tag: str) -> str:
-        output = ''
+            bridge_cb_decl += f"{cb} {cb + "_instance"} = NULL;\n"
+            self._callbacks[cb] = cb + "_instance"
+        self._code['BRIDGE_CALLBACK_DECL'] = bridge_cb_decl
 
-        if gen_tag == 'BRIDGE_INCLUDE':
-            header_name = os.path.basename(self._cfg["header"])
-            output += f'#include "{header_name}"\n'
+        #BRIDGE_CALLBACK_IMPL
 
-        elif gen_tag == 'BRIDGE_CALLBACK_DECL':
-            for cb_type, cb_name in self._callbacks.items():
-                output += f"{cb_type} {cb_name} = NULL;\n"
-
-        elif gen_tag == 'BRIDGE_CALLBACK_IMPL':
-            pass
-
-        elif gen_tag == 'BRIDGE_IMPL':
-            pass
-
-        else:
-            assert False
-        
-        return output
